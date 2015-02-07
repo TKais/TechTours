8bd87d66b3c7d41e638f84fa773a4b8cde6d8378
TKais <TiffanyTKaiser@gmail.com>
2015-02-06 16:41:31 -0800

    Rearrange javascript so it's MVC -- still working on that

--- app.js
+++ app.js
@@ -21,3 +21,25 @@ app.get('/', function(req, res){
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
+
+
+
+function initialize() {
+  var mapOptions = {
+    zoom: 8,
+    center: new google.maps.LatLng(-34.397, 150.644)
+  };
+
+  var map = new google.maps.Map(document.getElementById('map-canvas'),
+      mapOptions);
+}
+
+function loadScript() {
+  var script = document.createElement('script');
+  script.type = 'text/javascript';
+  script.src = 'https://maps.googleapis.com/maps/api/js?v=3.exp&' +
+      'callback=initialize';
+  document.body.appendChild(script);
+}
+
+window.onload = loadScript; 
