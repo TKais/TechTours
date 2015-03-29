b69b044610bba86d78358aa8d92cc01f8f3bd722
TKais <TiffanyTKaiser@gmail.com>
2015-03-28 23:07:08 -0700

    Clean up server by using app.listen only

--- app.js
+++ app.js
@@ -7,29 +7,13 @@ var geohash = require('geohash').GeoHash;
 
 
 var app = express();
-app.set('port', process.env.PORT || 3000);
 app.set('views', path.join(__dirname, 'views'));
 
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.listen(3000);
+
 app.get('/', function(req, res){
   res.render('index.ejs');
 });
-
-
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
-});
-
-
-
-function initialize() {
-  var mapOptions = {
-    zoom: 8,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
-
-  var map = new google.maps.Map(document.getElementById('map-canvas'),
-      mapOptions);
-}
