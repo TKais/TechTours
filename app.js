8161ecd98ac7ad28d2377636f30e2e1e66e9f1b3
TKais <TiffanyTKaiser@gmail.com>
2015-04-07 18:37:44 -0700

    Pass listen function a callback so we can see logging when the server is running

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.listen(3000);
+app.listen(3000, function(){
+  console.log("Server listening on localhost:3000");
+});
 
 app.get('/', function(req, res){
   res.render('index.ejs');
